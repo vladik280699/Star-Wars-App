7d3546468b3d84c16a5274336a22e27f28b6c593
Vlad <vladik280699@gmail.com>
2020-02-08 19:22:25 +0200

    Updates

---
+++ precache-manifest.16c9f5cbf6f3037f75d9814d40fe5abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671566a5b70dd03a46ae887716183bf9",
+    "url": "/Star-Wars-App/index.html"
+  },
+  {
+    "revision": "42c877e382ef21851a04",
+    "url": "/Star-Wars-App/static/css/main.c198e54b.chunk.css"
+  },
+  {
+    "revision": "fc8efb475f434d12807f",
+    "url": "/Star-Wars-App/static/js/2.cbafb18d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Star-Wars-App/static/js/2.cbafb18d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c877e382ef21851a04",
+    "url": "/Star-Wars-App/static/js/main.61c85247.chunk.js"
+  },
+  {
+    "revision": "fb14d018272da35e8171",
+    "url": "/Star-Wars-App/static/js/runtime-main.7d02cfe2.js"
+  }
+]);
